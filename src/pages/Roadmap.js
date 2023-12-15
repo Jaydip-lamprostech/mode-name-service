9c0a512c5d32629feb23ed85f098869010ecd999
purvik6062 <purvikpanchal6336@gmail.com>
2023-12-15 14:07:29 +0530

    useEffect on roadmap page

--- src/pages/Roadmap.js
+++ src/pages/Roadmap.js
@@ -1,7 +1,17 @@
 import React from "react";
 import RoadmapSection from "../components/RoadmapSection";
+import { useEffect } from "react";
 
 function Roadmap() {
+  useEffect(() => {
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+      event: 'pageview',
+      pagePath: window.location.pathname,
+      pageTitle: "Roadmap",
+      location: window.location.href,
+    });
+  }, []);
   return <>
     <RoadmapSection />
   </>
